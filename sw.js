4f8f95113c0620439f78ea3641bad789b7350a25
fedirjh <36869046+fedirjh@users.noreply.github.com>
2018-06-28 22:53:52 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+var staticCacheName = 'converter-v1';
+
+self.addEventListener('install', function(event) {
+  // TODO: cache /skeleton rather than the root page
+
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'idb.js',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('wittr-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+  var apiurlcur = new URL('https://free.currencyconverterapi.com/api/v5/currencies');
+  if(requestUrl === apiurlcur){
+      event.respondWith(caches.match('/api/v5/currencies'));
+      return;
+  }
+  event.respondWith(
+    caches.match(event.request.url).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
