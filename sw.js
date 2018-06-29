738f668af29d57096fd12169182b6715971d40bd
fedirjh <36869046+fedirjh@users.noreply.github.com>
2018-06-29 01:08:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -32,8 +32,16 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
   var requestUrl = new URL(event.request.url);
   var apiurlcur = new URL('https://free.currencyconverterapi.com/api/v5/currencies');
+  var apiurl = new URL('https://free.currencyconverterapi.com/api/v5/convert?q='+querySt('q')+'&compact=y');
   if(requestUrl === apiurlcur){
       event.respondWith(caches.match('/api/v5/currencies'));
+      return;
+  }
+  if(requestUrl === apiurl){
+    dbPromise.then(db => {
+    return db.transaction('objs')
+      .objectStore('mycurrency').get(querySt('q'));
+  }).then(allObjs => event.respondWith((allObjs));
       return;
   }
   event.respondWith(
@@ -48,3 +56,13 @@ self.addEventListener('message', function(event) {
     self.skipWaiting();
   }
 });
+function querySt(Key) {
+    var url = event.request.url;
+    KeysValues = url.split(/[\?&]+/);
+    for (i = 0; i < KeysValues.length; i++) {
+        KeyValue = KeysValues[i].split("=");
+        if (KeyValue[0] == Key) {
+            return KeyValue[1];
+        }
+    }
+}
