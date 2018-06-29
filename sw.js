5aae4a5d62a39d3e7f5ad8082cbf29d20c9264e1
fedirjh <36869046+fedirjh@users.noreply.github.com>
2018-06-29 01:35:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -33,25 +33,16 @@ self.addEventListener('fetch', function(event) {
   var requestUrl = new URL(event.request.url);
   var apiurlcur = new URL('https://free.currencyconverterapi.com/api/v5/currencies');
   var apiurl = new URL('?q='+querySt('q')+'&compact=y');
-function querySt(Key) {
-    var url = event.request.url;
-    KeysValues = url.split(/[\?&]+/);
-    for (i = 0; i < KeysValues.length; i++) {
-        KeyValue = KeysValues[i].split("=");
-        if (KeyValue[0] == Key) {
-            return KeyValue[1];
-        }
-    }
-}
+
   if(requestUrl === apiurlcur){
       event.respondWith(caches.match('/api/v5/currencies'));
       return;
   }
   if(requestUrl.origin === 'https://free.currencyconverterapi.com'){
     if( requestUrl.pathname === '/api/v5/convert'){
-      if(querySt('q')){
+      if(event.request.url,querySt('q')){
     dbPromise.then(db => {
-    return db.transaction('objs').objectStore('mycurency').get(querySt('q'));
+    return db.transaction('objs').objectStore('mycurency').get(event.request.url,querySt('q'));
     }).then(allObjs => event.respondWith((allObjs));
   }
   else{
@@ -73,3 +64,12 @@ self.addEventListener('message', function(event) {
   }
 });
 
+function querySt(url,Key) {
+    KeysValues = url.split(/[\?&]+/);
+    for (i = 0; i < KeysValues.length; i++) {
+        KeyValue = KeysValues[i].split("=");
+        if (KeyValue[0] == Key) {
+            return KeyValue[1];
+        }
+    }
+}
